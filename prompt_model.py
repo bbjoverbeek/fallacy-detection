b1b895fc7a57d465525a7b4f3933ff8830edc7ba
Björn Overbeek <bbjoverbeek@gmail.com>
2024-04-29 23:36:06 +0200

    Improve prompting script

--- prompt_model.py
+++ prompt_model.py
@@ -1,37 +1,127 @@
 import argparse
+import json
 from transformers import pipeline
 from datasets import load_dataset, Dataset
 from dataclasses import dataclass
-from enum import Enum
+from typing import Literal
+from tqdm import tqdm
 
-PromptTechnique = Enum('PromptTechnique', ['zero-shot', 'few-shot', 'chain-of-thought', 'self-consistency', 'positive_feedback'])
+# TODO: find way to truncate the generated text if too long for the model
 
 @dataclass
-class Prompt:
-    """A dataclass to hold the prompts and their corresponding labels"""
+class Fallacy:
+    """A dataclass to hold the fally text and their corresponding labels."""
+    text: str
+    label: str
+
+    def __str__(self) -> str:
+        return f'Text: "{self.text}"\nLabel: "{self.label}"'
+
+    def get_zero_shot_prompt(self, fallacy_options: list[str]) -> str:
+        """Return the zero-shot prompt"""
+
+        prompt = f'What logical fallacy is used here?"\n{self.text}"\nThe options are: {fallacy_options}.'
+
+        return prompt
+
+    def get_few_shot_prompt(self, fallacy_options: list[str], n_shot: int) -> str:
+        """Return a few-shot prompt"""
+        raise NotImplementedError
+
+    def get_chain_of_thought_prompt(self, fallacy_options: list[str]) -> str:
+        """Return a chain-of-thought prompt"""
+        prompt = f'{self.get_zero_shot_prompt(fallacy_options)}\nLet\'s think in steps'
+
+    # Note that the self-consistency prompt is the same as the chain of thought prompt
+    
+    def get_positive_feedback_prompt(self, fallacy_options: list[str], sentiment: Literal['positive', 'negative']) -> str:
+        """Return a positive-feedback prompt"""
+
+        if sentiment == 'positive':
+            prompt = f'Good job on that last prompt! Let\'s try another one. {self.get_zero_shot_prompt(fallacy_options)}'
+        elif sentiment == 'negative':
+            prompt = f'That last attempt was horrendous! Try this one instead. {self.get_zero_shot_prompt(fallacy_options)}'
+
+        return prompt
+
 
 def parse_args() -> argparse.Namespace:
     """Parse the command-line arguments"""
 
     parser = argparse.ArgumentParser(description="Prompt LLMs for fallacy detection")
 
-    parser.add_argument('-m','--model', type=str, default='google/flan-t5-small', help='Model to prompt',)
+    parser.add_argument(
+        '-m',
+        '--model', 
+        type=str, 
+        default='google/flan-t5-small', 
+        help='Model to prompt'
+    )
 
-    parser.add_argument('-d','--dataset', type=str, default='data/dev/fallacy_corpus.jsonl', help='Fallacy dataset to prompt for',)
+    parser.add_argument(
+        '-d',
+        '--dataset',
+        type=str,
+        default='data/dev/fallacy_corpus.jsonl', 
+        help='Fallacy dataset to prompt for'
+    )
+
+    parser.add_argument(
+        '-l',
+        '--logpath',
+        default='',
+        type=str,
+        help='Log file to write the results to'
+    )
 
     return parser.parse_args()
 
-def load_dataset(dataset: str, prompt_technique: Prompt) -> Dataset:
-    pass
+
+def load_dataset(dataset_path: str) -> list[Fallacy]:
+    """Convert jsonl lines to a list of prompts"""
+
+    fallacies = []
+
+    with open(dataset_path, 'r') as inp:
+        dataset = inp.readlines()
+
+    for line in dataset:
+        example = json.loads(line)
+        fallacies.append(Fallacy(example['text'], example['labels'][0][2]))
+    
+    return fallacies
 
 
-def prompt_model(pipe: pipeline, dataset: Dataset) -> None:
-    """Prompt the model for fallacy detection on the dataset"""
-    dataset = ['Is the following a fallacy, and if yes, what fallacy is it?: "Understood, I would agree.  I\'m posting it because it\'s from the BBC so I expect it to be well sourced by them."']
+def prompt_model(pipe: pipeline, fallacies: list[Fallacy], logpath: str) -> list[str]:
+    """Prompt the model for all fallacies"""
 
-    for example in dataset:
-        result = pipe(example, do_sample=True, temperature=0.7, max_new_tokens=100)
-        print(result)
+    fallacy_options = {fallacy.label for fallacy in fallacies}
+
+    generated_texts = []
+    for fallacy in tqdm(fallacies, desc='Prompting model', leave=False):
+        generated_text = pipe(
+            fallacy.get_zero_shot_prompt(fallacy_options), 
+            do_sample=True, 
+            temperature=0.7, 
+            top_k=40, 
+            max_new_tokens=200
+        )
+        generated_texts.append(generated_text[0]['generated_text'])
+
+    return generated_texts
+
+
+def evaluate_generated_texts(fallacies: list[Fallacy], generated_texts: list[str]) -> None:
+    """Checks if the model prediction is correct, and prints the number of correct predictions."""
+    
+    correct = 0
+
+    for fallacy, generated_text in zip(fallacies, generated_texts):
+        # TODO improve the comparison code below
+        if fallacy.label in generated_text:
+            correct += 1
+
+    print(f'Got {correct} out of {len(fallacies)} correct')
 
 
 def main() -> None:
@@ -39,10 +129,18 @@ def main() -> None:
 
     args = parse_args()
 
-    pipe = pipeline('text2text-generation', model='google/flan-t5-small')
+    pipe = pipeline('text2text-generation', model=args.model)
 
-    prompt_model(pipe, [])
+    fallacies = load_dataset(args.dataset)
 
+    generated_texts = prompt_model(pipe, fallacies, args.logpath)
+
+    if args.logpath:
+        with open(args.logpath, 'w', encoding='utf-8') as outp:
+            for fallacy, generated_text in zip(fallacies, generated_texts):
+                outp.write(f'{fallacy}\nGenerated text: "{generated_text}"\n\n')
+
+    evaluate_generated_texts(fallacies, generated_texts)
     
 if __name__ == '__main__':
     main()
